005976ee14d4bb7c35d6987e3bd3a647e5853c7c
rafa <rafa.zamora.rals@gmail.com>
2019-11-29 23:59:46 -0600

    Se borro la parte de Cuadro.hpp

--- PacMan/Graphics_Engine/Cuadro.hpp
+++ PacMan/Graphics_Engine/Cuadro.hpp
@@ -63,45 +63,4 @@ public:
     void pintaSolido();
 };
 
-#if 0
-Cuadro::Cuadro(/* args */)
-{
-}
-
-Cuadro::Cuadro(int x, int y, int colorSolido, int colorDecora)
-{
-    
-    this->setPosicion(x,y);
-    this->setColorSolido(colorSolido);
-    this->setColorDecora(colorDecora);
-}
-
-void Cuadro::pinta(){
-    //Se cambia el color para pintar en el color Solido del cuadro
-    miniwin::color(getColorSolido());
-    miniwin::rectangulo_lleno(   MARGEN + 1 + this->getPosicion().x * TAM,
-                        MARGEN + 1 + this->getPosicion().y * TAM,
-                        MARGEN + this->getPosicion().x * TAM + TAM,
-                        MARGEN + this->getPosicion().y * TAM + TAM);
-    //Se cambia el color para pintar en el color Decora del cuadro
-    miniwin::color(getColorDecora());
-    miniwin::rectangulo( MARGEN + 3 + this->getPosicion().x * TAM,
-                MARGEN + 3 + this->getPosicion().y * TAM,
-                MARGEN + this->getPosicion().x * TAM + TAM - 3,
-                MARGEN + this->getPosicion().y * TAM + TAM - 3);
-}
-
-void Cuadro::pintaSolido(){
-    //Se cambia el color para pintar en el color Solido del cuadro
-    miniwin::color(getColorSolido());
-    miniwin::rectangulo_lleno(   MARGEN + 1 + this->getPosicion().x * TAM,
-                        MARGEN + 1 + this->getPosicion().y * TAM,
-                        MARGEN + this->getPosicion().x * TAM + TAM,
-                        MARGEN + this->getPosicion().y * TAM + TAM);
-}
-
-Cuadro::~Cuadro()
-{
-}
-#endif
 #endif  //CUADRO_HPP
